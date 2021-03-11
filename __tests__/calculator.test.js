97f623543880d82e5bc8cca7c0104e0993fa6e33
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-11 11:43:41 -0600

    pass first test: create object calculator

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,6 +1,6 @@
 import {Calculator} from './../src/calculator.js';
 describe ('Calculator', () => {
  test ('should create a calculator object', () => {
-   const calculator = new Calculator();
+   const calculator = new Calculator(32);
   expect(calculator.age).toEqual(32);
 })});
