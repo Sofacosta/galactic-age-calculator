b17ffd86c13e7aefb97b3f4a477bf5c5ff86b3fc
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-11 11:38:14 -0600

    fail first test: create object calculator

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,1 +1,6 @@
-//import {Calculator} from './../src/calculator.js';
+import {Calculator} from './../src/calculator.js';
+describe ('Calculator', () => {
+ test ('should create a calculator object', () => {
+   const calculator = new Calculator();
+  expect(calculator.age).toEqual(32);
+})});
--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,10 @@
+export class Calculator {
+  constructor(age, expectancy) {
+    this.age = age;
+    this.expectancy = expectancy;
+  }
+}
+
 
 // Steps:
 // 1 Create a calcualtor constructor with two properties of age and expectency 
