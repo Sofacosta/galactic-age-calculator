65ba5354221a30990e0298e925b4d5966eb6a82c
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-24 21:28:17 -0600

    add function to calculate if user has already surpassed life exoectancy in calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,8 @@ export class Calculator {
     this.age = age;
     this.expectancy = expectancy;
     this.planetAge = this.planetAgeCalculator(); 
-    this.planetExpectancy = this.planetExpectancyCalculator(this.planetAgeCalculator());
+    this.planetExpectancy = this.planetExpectancyCalculator(this.planetAge);
+    this.hasUserSurpassedLifeExpectancy = this.hasUserSurpassedLifeExpectancy(this.planetExpectancy);
   }
   planetAgeCalculator() {
     const mercury = .24;
@@ -31,9 +32,14 @@ export class Calculator {
     return planetExpectancy;
   }
 
+  hasUserSurpassedLifeExpectancy(planetExpectancy) {
+    let planetsOnWhichUserHasSurpassedLifeExpectancy = {};
+    for (let key in planetExpectancy) {
+      if (Math.sign(planetExpectancy[key]) === -1) {
+        planetsOnWhichUserHasSurpassedLifeExpectancy[key] = "You're " + (planetExpectancy[key] * -1) + " years past your life expectancy on " + key;
+      }
+    }
+    console.log(this.hasUserSurpassedLifeExpectancy(planetExpectancy));
+    return planetsOnWhichUserHasSurpassedLifeExpectancy;
+  }
 }
-
-// UI
-
-
-
