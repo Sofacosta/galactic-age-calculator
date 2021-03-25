fe02d133b63cc4cce128d822a17186f0670da602
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-24 21:29:29 -0600

    add final test, however not working yet

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -11,7 +11,11 @@ describe('Calculator', () => {
   });
 
   test('should create a planet calculator returning your planet age', () => {
-    expect(calculator.planetAgeCalculator()).toEqual({ mercury: 133.33333333333334, venus: 50, mars: 17.02127659574468, jupiter: 2.69814502529511 });
+    expect(calculator.planetAgeCalculator()).toEqual({ 
+      mercury: 133.33333333333334,
+      venus: 50,
+      mars: 17.02127659574468,
+      jupiter: 2.69814502529511 });
   }); 
 
   test('should calculate life expectency in each planet', () => {
@@ -24,7 +28,11 @@ describe('Calculator', () => {
     });
   });
 
-  //test ('If a user has already surpassed the average life expectancy, return the number of years they have lived past the life expectancy', () => {
-  
+  test('If a user has already surpassed the average life expectancy, return the number of years they have lived past the life expectancy', () => {    
+    expect(calculator.hasUserSurpassedLifeExpectancy(planetExpectancy)).toEqual({
+      mercury: -53.33333333333334, 
+      venus: 30, 
+      mars: 62.97872340425532, 
+      jupiter: 77.3018549747049,
+  });  
 });
-
